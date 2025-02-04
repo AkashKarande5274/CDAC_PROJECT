a56841f9745cc29096b6d6fec354df9d7ecafae3
Akash Karande <akashkarande5274@gmail.com>
2025-02-04 15:52:58 +0530

    Backend if one unit

---
+++ Backend/routes/StudyMaterial.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const mysql = require('mysql2');
+const config = require('config');
+const router = express.Router();
+
+const connectionDetails = {
+    host: config.get('host'),
+    user: config.get('user'),
+    password: config.get('password'),
+    database: config.get('database'),
+};
+
+
+
+router.get('/studymaterial', (req, res) => {
+    
+    const sql = `SELECT  *   FROM  studymaterial`; // SQL Query with placeholder
+
+    console.log(sql);
+
+    // Create a new connection
+    const connection = mysql.createConnection(connectionDetails);
+
+    connection.connect((connectError) => {
+        if (connectError) {
+            console.error('Database connection error:', connectError.message);
+            res.status(500).json({
+                status: 'error',
+                message: 'Database connection failed',
+                errorMessage: connectError.message
+            });
+            return;
+        }
+
+        // Query the database
+        connection.query(sql,  (queryError, result) => {
+            if (queryError) {
+                console.error('Database query error:', queryError.message);
+                res.status(500).json({
+                    status: 'error',
+                    message: 'Something went wrong...',
+                    errorMessage: queryError.message
+                });
+            } else if (result.length === 0) {
+                res.status(404).json({
+                    status: 'error',
+                    message: `No documents found`
+                });
+            } else {
+                res.status(200).json({
+                    status: 'success',
+                    data: result
+                });
+            }
+
+            // End the connection after query
+            connection.end((endError) => {
+                if (endError) {
+                    console.error('Error closing the connection:', endError.message);
+                }
+            });
+        });
+    });
+});
+
+
+module.exports = router;
